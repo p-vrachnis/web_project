62bada2a8345aa1e468d7aaf2aa3a270536e8a87
Dimitris Sarantakis <sarantakis_dimitris@hotmail.com>
2020-03-10 00:55:50 +0200

    beautify

--- main/main.php
+++ main/main.php
@@ -10,10 +10,10 @@ $_SESSION['loggedin']=false;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crowdsourcing System</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <link href="/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
+    <script src="/external/jquery-3.4.1.js"></script>
+    <link href="/external/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="/main/main.css">
-    <link rel="stylesheet" href="/fontawesome/css/all.css">
+    <link rel="stylesheet" href="/external/fontawesome/css/all.css">
 </head>
 
 <body>
