29ce792d68cd190cd2d9b664ec745fe1800d33d5
Dimitris Sarantakis <sarantakis_dimitris@hotmail.com>
2020-03-11 12:42:54 +0200

    fix md5 char length

--- web_project.sql
+++ web_project.sql
@@ -4,12 +4,12 @@ use web_project;
 
 
 CREATE TABLE users(
-  userID CHAR(128) NOT NULL,
+  userID CHAR(32) NOT NULL,
   username VARCHAR(25) NOT NULL,
-  password VARCHAR (50) NOT NULL,
+  password CHAR(32) NOT NULL,
   email VARCHAR(50) NOT NULL,
   PRIMARY KEY(username)
 )ENGINE = InnoDB CHARACTER SET greek COLLATE greek_general_ci;
 
 INSERT INTO users VALUES 
-('1','admin','1234','admin@hotmail.com');
+('1','admin','81dc9bdb52d04dc20036dbd8313ed055','admin@hotmail.com');
