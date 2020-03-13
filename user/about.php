afd6d5d0946b0a250755251b55301869575147f6
Dimitris Sarantakis <sarantakis_dimitris@hotmail.com>
2020-03-13 16:12:02 +0200

    include page.php for header

--- user/about.php
+++ user/about.php
@@ -1,5 +1,6 @@
 <?php
   include "../session.php";
+  include "./page.php";
   $username = $_SESSION['login_user'];
 ?>
 
@@ -8,22 +9,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Page</title>
     <script src="/external/jquery-3.4.1.js"></script>
     <link href="/external/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="./css/about.css">
+    <title>About Page</title>
 </head>
 
 <body>
     <div class="design">
-      <div class="header">
-        <a class="logo" href="./home.php"><img src= "../images/logo.png">Welcome user <?php echo $username ?></a>
-        <div class="header-right">
-          <a href="./home.php">Home</a>
-          <a class="active" href="./about.php">About</a>
-          <a href='./logout.php'>Logout</a>
-        </div>
-      </div>
       <div class="main_content">
         <div class = "about">
           <p id="uni_logo"><img src= "../images/uni_logo.png"> </p>
--- user/home.php
+++ user/home.php
@@ -1,5 +1,6 @@
 <?php
   include "../session.php";
+  include "./page.php";
   $username = $_SESSION['login_user'];
 ?>
 
@@ -8,22 +9,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
     <script src="/external/jquery-3.4.1.js"></script>
     <link href="/external/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="./css/home.css">
+    <title>Home Page</title>
 </head>
 
 <body>
     <div class="design">
-      <div class="header">
-        <a class="logo" href="./home.php"><img src= "../images/logo.png">Welcome user <?php echo $username ?></a>
-        <div class="header-right">
-          <a class="active" href="./home.php">Home</a>
-          <a href="./about.php">About</a>
-          <a href='./logout.php'>Logout</a>
-        </div>
-      </div>
       <div class="main_content">
         <div class="user_info">
           <p style="text-align: center">Απεικόνιση στοιχείων χρήστη</p>
