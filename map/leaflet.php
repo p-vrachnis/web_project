5ebeb105c75fee9e48c117bcec78b9335f84d7d6
Dimitris Sarantakis <sarantakis_dimitris@hotmail.com>
2020-03-15 23:52:17 +0200

    lasso

--- map/leaflet.php
+++ map/leaflet.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="/external/jquery-3.4.1.js"></script>
-    <link href="/external/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
+    <script src="../external/jquery-3.4.1.js"></script>
+    <link href="../external/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="../map/css/leaflet.css">
     <link rel="stylesheet" href="../external/leaflet/leaflet.css" />
     <script src="../external/jquery-3.4.1.js"></script>
@@ -17,10 +17,20 @@
     <button id="save">Save</button>
     <button id="load">Load</button>
   </div>
-    <div id = "map"></div>
+    <div id = "map">
+      <div class="leaflet-top leaflet-right">
+        <div class="leaflet-bar leaflet-control">
+          <a class="leaflet-control-lasso" id="leaflasso" href="#" title="Toggle Lasso" role="button" aria-label="Toggle Lasso"></a>
+        </div>
+      </div>
+    </div>
 
     <script src="../external/leaflet/leaflet.js"></script>
+    <script src="https://unpkg.com/leaflet-lasso@2.0.6/dist/leaflet-lasso.umd.min.js"></script>
     <script src="../map/leaflet.js"></script>
+    <script type="text/typescript" src="../map/leaflet.ts"></script>
 
+    <script src="../external/typescript.min.js"></script>
+    <script src="../external/typescript.compile.min.js"></script>
 </body>
 </html>
---
+++ map/leaflet.ts
@@ -0,0 +1,25 @@
+// Lasso
+// Handler
+interface LassoHandlerOptions {
+   polygon?: L.PolylineOptions,
+   intersect?: boolean;
+}
+const lasso = L.lasso(map, mapOptions);
+leaflasso.addEventListener('click', () => {
+    lasso.enable();
+});
+
+// control
+type LassoControlOptions = LassoHandlerOptions & L.ControlOptions;
+
+L.control.lasso(options).addTo(map);
+
+// Finished event
+interface LassoHandlerFinishedEventData {
+   latLngs: L.LatLng[];
+   layers: L.Layer[];
+}
+
+map.on('lasso.finished', (event: LassoHandlerFinishedEventData) => {
+   console.log(event.layers);
+});
