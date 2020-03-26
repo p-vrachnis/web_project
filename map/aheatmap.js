5b232e81f01fdc8e9605748f5a9aa18097494cb9
ZachGeo <zachageo95@gmail.com>
2020-03-26 19:48:42 +0200

    Heatmap Show

--- map/aheatmap.js
+++ map/aheatmap.js
@@ -1,24 +1,29 @@
-let baseLayer = L.tileLayer(
-    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a>'
+    // Create the base Leaflet layer (the map itself)
+    let baseLayer = L.tileLayer(
+      'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a>'
+      }
+    )
+
+    // Configure and create the heatmap.js layer
+    let cfg = {
+        "radius": 30,
+        "useLocalExtrema": true,
+        "opacity": 0.5
     }
-)
 
-let cfg = {
-    "radius": 30,
-    "useLocalExtrema": true,
-    "opacity": 0.5
-};
-let heatmapLayer = new HeatmapOverlay(cfg);
+    let heatmapLayer = new HeatmapOverlay(cfg)
 
-let propertyHeatMap = new L.Map("heatmap", {
-    center: new L.LatLng(38.230462, 21.753150),
-    zoom: 13.3,
-    layers: [baseLayer, heatmapLayer]
-})
+    // Create the overall Leaflet map using the two layers we created
+    let propertyHeatMap = new L.Map('map', {
+      center: new L.LatLng(38.230462, 21.753150),
+      zoom: 13.3,
+      layers: [baseLayer, heatmapLayer]
+    })
 
-var testData = {
-    data: [] //data_heatmap
-};
-
-heatmapLayer.setData(testData);
+    var testData = {
+        data: [{ lat:38.230462, lng:21.753150}] //data_heatmap
+    };
+    
+    // Add data (from sales.js file) to the heatmap.js layer
+    heatmapLayer.setData(testData);
--- map/aheatmap.php
+++ map/aheatmap.php
@@ -9,19 +9,21 @@
     <script src="../external/jquery-3.4.1.js"></script>
     <link href="../external/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="../map/css/aheatmap.css">
-    <link rel="stylesheet" href="../external/leaflet/leaflet.css" />
-    <script src="../external/jquery-3.4.1.js"></script>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.5.1/leaflet.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css" />
+
     <title>HeatMap</title>
 </head>
 
 <body>
 
-    <div id="heatmap"></div>
+    <div id="map"></div>
 
     <script src="../external/node_modules/heatmap.js/build/heatmap.min.js"></script>
     <!-- ΔΕΝ ΤΟ ΕΧΟΥΜΕ ΚΑΤΕΒΑΣΕΙ !! -->
     <script src="https://cdn.jsdelivr.net/npm/leaflet-heatmap@1.0.0/leaflet-heatmap.js"></script>
-    <script src="../map/heatmap.js"></script>
+    <script src="../map/aheatmap.js"></script>
 
 </body>
 </html>
