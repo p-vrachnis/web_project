95e05f15730a3e80fc7d5417deab04159378905e
user.apr0vleptos <sarantakis_dimitris@hotmail.com>
2020-03-28 15:04:39 +0200

    login/register 3tab fix

--- main/main.js
+++ main/main.js
@@ -5,9 +5,14 @@ var y = document.getElementById("register");
 function register(){
     x.style.left = "-1000px";
     y.style.left = "0px";
+    x.style.visibility = "hidden";
+    y.style.visibility = "visible";
 }
 
 function login(){
     x.style.left = "0px";
     y.style.left = "1000px";
+    y.style.visibility = "hidden";
+    x.style.visibility = "visible";
+
 }
