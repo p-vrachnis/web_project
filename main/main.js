c0ba504124ca5e9d6179c0716c7825296d0dcfdd
ZachGeo <zachageo95@gmail.com>
2020-03-29 14:22:43 +0300

    Show/Hide Login-Register Form

--- main/main.js
+++ main/main.js
@@ -1,18 +1,14 @@
 // Change box-1 between LogIn & Register
-var x = document.getElementById("login");
-var y = document.getElementById("register");
+$(function () {
+    $("#register_btn").click(function () {
+        $("#register").show();
+        $("#login").hide();
+    });
+});
 
-function register(){
-    x.style.left = "-1000px";
-    y.style.left = "0px";
-    x.style.visibility = "hidden";
-    y.style.visibility = "visible";
-}
-
-function login(){
-    x.style.left = "0px";
-    y.style.left = "1000px";
-    y.style.visibility = "hidden";
-    x.style.visibility = "visible";
-
-}
+$(function () {
+    $("#login_btn").click(function () {
+        $("#register").hide();
+        $("#login").show();
+    });
+});
